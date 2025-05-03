836024cb75176184d09ac62c4ab9509431333c28
Klodjan Mata <klodjanmata0@gmail.com>
2025-05-03 15:26:24 +0200

    testing

---
+++ src/main/java/JavaAdv/Exercises/Task3/Main.java
@@ -0,0 +1,11 @@
+package JavaAdv.Exercises.Task3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+        shape = new Shape("White", true);
+        System.out.println(shape);
+    }
+}
--- src/main/java/JavaAdv/Exercises/Task3/Shape.java
+++ src/main/java/JavaAdv/Exercises/Task3/Shape.java
@@ -1,4 +1,44 @@
 package JavaAdv.Exercises.Task3;
 
-public class Shape {
+public class Shape{
+    private String color;
+    private boolean isFilled;
+
+    public Shape(String color, boolean isFilled){
+        this.color=color;
+        this.isFilled=isFilled;
+    }
+
+    public Shape(){
+        color = "unknown";
+        isFilled = false;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return isFilled;
+    }
+
+    public void setFilled(boolean filled) {
+        isFilled = filled;
+    }
+
+    @Override
+    public String toString(){
+        return "Shape with color of " + color + " and " + (isFilled ? "filled" : "not filled");
+//        if (isFilled) {
+//            "filled"
+//        }
+//        else{
+//            "not filled"
+//        }
+    }
+
 }
