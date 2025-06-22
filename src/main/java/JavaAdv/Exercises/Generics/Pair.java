247aabac487d314ecefa0d8632e60e01889b3382
Klodjan Mata <klodjanmata0@gmail.com>
2025-06-22 12:44:27 +0200

    Generics

--- src/main/java/JavaAdv/Exercises/Generics/Pair.java
+++ src/main/java/JavaAdv/Exercises/Generics/Pair.java
@@ -1,4 +1,27 @@
 package JavaAdv.Exercises.Generics;
 
-public class Pair {
+public class Pair<T, U> {
+    private T firstItem;
+    private U secondItem;
+
+    public Pair(T firstItem, U secondItem) {
+        this.firstItem = firstItem;
+        this.secondItem = secondItem;
+    }
+
+    public T getFirstItem() {
+        return firstItem;
+    }
+
+    public void setFirstItem(T firstItem) {
+        this.firstItem = firstItem;
+    }
+
+    public U getSecondItem() {
+        return secondItem;
+    }
+
+    public void setSecondItem(U secondItem) {
+        this.secondItem = secondItem;
+    }
 }
---
+++ src/main/java/JavaAdv/Exercises/Generics/Task4/Book.java
@@ -0,0 +1,6 @@
+package JavaAdv.Exercises.Generics.Task4;
+
+public class Book {
+    private String title;
+    private String author;
+}
