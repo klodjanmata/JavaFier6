843e930c50d5571748800191c5ea314cc53d2954
Klodjan Mata <klodjanmata0@gmail.com>
2025-06-28 10:55:35 +0200

    Java ADV Coding EX 1, 2

---
+++ src/main/java/JavaAdvCoding/Exercise1.java
@@ -0,0 +1,50 @@
+package JavaAdvCoding;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("spinach");
+        fruits.add("Date");
+        fruits.add("Banana");
+        fruits.add("apple");
+        fruits.add("Elderberry");
+        fruits.add("Cherry");
+        List<String> orderedFruits = reverseOrderCaseInsensitiveMap(fruits);
+        System.out.println(orderedFruits);
+    }
+    // Exercise 1
+    public static List<String> reverseOrder(List<String> list){
+        return list.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    // Exercise 2
+    public static List<String> reverseOrderCaseInsensitive(List<String> list){
+        List<String> orderedList = new ArrayList<>();
+        for (String s : list) {
+            orderedList.add(s.toUpperCase());
+        }
+        return orderedList.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> reverseOrderCaseInsensitiveMap(List<String> list){
+        Map<String, String> orderedMap = new HashMap<>();
+        for (String s : list) {
+            orderedMap.put(s.toUpperCase(), s);
+        }
+        List<String> orderedKeys = orderedMap.keySet().stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        List<String> listToReturn = new ArrayList<>();
+        for (String s : orderedKeys) {
+            listToReturn.add(orderedMap.get(s));
+        }
+        return listToReturn;
+    }
+}
